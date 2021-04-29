41637d379c839c8c16c1ccc5f70f1168996b3e24
최종명 <zmzhoi@gmail.com>
2021-04-29 14:39:12 +0900

    feat: 글로벌 스타일 설정 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './libs/styles/global';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div>App</div>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;

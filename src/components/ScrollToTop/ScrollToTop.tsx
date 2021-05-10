9ba2fc6e5bd8f6fe89db678258cfcabb8ffee2a5
최종명 <zmzhoi@gmail.com>
2021-05-10 11:05:54 +0900

    fix: ScrollToTop 오작동 수정
    * 뒤로가기 시 스크롤 올리는 동작 방지 처리
    (history.action === 'PUSH'일 경우에만 수행)

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,15 +1,18 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 interface IProps {}
 
 function ScrollToTop({}: IProps) {
+  const history = useHistory();
   const { pathname } = useLocation();
 
   useEffect(() => {
     // history.push로 페이지 이동시 마다 스크롤을 최상단으로 ~
-    (window as any).scrollTo(0, 0);
-  }, [pathname]);
+    if (history.action === 'PUSH') {
+      (window as any).scrollTo(0, 0);
+    }
+  }, [pathname, history]);
 
   return null;
 }
